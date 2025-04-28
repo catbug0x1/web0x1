571e74cb59f73808c5342f7bb7dfea35e416fd63
catbug0x1 <moelsherbinii@gmail.com>
2025-04-28 23:18:19 +0300

    base: "/toji0day/",

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/اسم-الريبو/",  // <-- هنا لازم تحط اسم الريبو بتاعك
   server: {
     host: "::",
     port: "8080",
